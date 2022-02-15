4fb52f81bd029d4a532ab0374d2c887b750df14c
DESKTOP-49P942V\chanwoong <dlcksdnd6785@naver.com>
2022-02-15 15:53:35 +0900

    로그를 남기기 위한 템플릿 콜백 패턴 적용

--- src/main/java/skillUp/advanced/app/V5/OrderControllerV5.java
+++ src/main/java/skillUp/advanced/app/V5/OrderControllerV5.java
@@ -1,29 +1,35 @@
 
 package skillUp.advanced.app.V5;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import skillUp.advanced.app.v4.OrderServiceV4;
+import skillUp.advanced.trace.callback.TraceCallback;
+import skillUp.advanced.trace.callback.TraceTemplate;
 import skillUp.advanced.trace.logTrace.LogTrace;
 import skillUp.advanced.trace.template.AbstractTemplate;
 
 @RestController
-@RequiredArgsConstructor
 public class OrderControllerV5 {
 
     private final OrderServiceV4 orderService;
-    private final LogTrace trace;
+    private final TraceTemplate template;
+
+    public OrderControllerV5(OrderServiceV4 orderService,LogTrace trace) {
+        this.orderService = orderService;
+        this.template = new TraceTemplate(trace);
+    }
+
 
     @GetMapping("/v5/request")
     public String request(String itemId){
-        AbstractTemplate<String> abstractTemplate= new AbstractTemplate(trace) {
+        return template.excute("OrderController request()", new TraceCallback<>() {
             @Override
-            protected String call() {
+            public String call() {
                 orderService.orderItem(itemId);
+
                 return "ok";
             }
-        };
-        return abstractTemplate.excute("hello");
+        });
     }
 }
